08d19aea5fba188c7c919bbcd2c5a14bb9ec047b
helmer <helmer@dalt.cl>
2024-04-19 16:36:30 -0400

    [UPDATE] Empty data

--- src/test/java/org/helmer/appmockito/ejemplos/services/ExamenServicesImplTest.java
+++ src/test/java/org/helmer/appmockito/ejemplos/services/ExamenServicesImplTest.java
@@ -14,6 +14,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 
 class ExamenServicesImplTest {
+
     ExamenRepository examenRepository;
     ExamenServicesImpl examenServices;
     PreguntaRepository preguntaRepository;
